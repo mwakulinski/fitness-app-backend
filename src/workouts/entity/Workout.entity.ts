6c8b2e854725823ca62c27ac357b753ea244e54b
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-12 10:44:11 +0200

    reafactor: change to meet IWorkout interface

--- src/workouts/entity/Workout.entity.ts
+++ src/workouts/entity/Workout.entity.ts
@@ -1,9 +1,10 @@
-import { IWorkout } from 'src/interfaces/interfaces';
+import { IWorkout, IWorkoutsType } from 'src/interfaces/interfaces';
 
 export class Workout implements IWorkout {
   id: number;
   title: string;
   description: string;
+  type: IWorkoutsType;
   duration: number;
   data: string;
 }
--- src/workouts/mockDataBase/mockData.ts
+++ src/workouts/mockDataBase/mockData.ts
@@ -1,3 +1,4 @@
+import { IWorkoutsType } from 'src/interfaces/interfaces';
 import { Workout } from '../entity/Workout.entity';
 
 export const mockDataBase: Workout[] = [
@@ -5,6 +6,7 @@ export const mockDataBase: Workout[] = [
     id: 1,
     title: 'Szybkie bieganie',
     description: '5 serii po 10 sekund sprintu x 3 serie',
+    type: IWorkoutsType.running,
     duration: 15,
     data: '2022-04-05',
   },
@@ -12,6 +14,7 @@ export const mockDataBase: Workout[] = [
     id: 2,
     title: 'Wolne bieganie',
     description: '30 minut jogingu',
+    type: IWorkoutsType.running,
     duration: 30,
     data: '2022-04-15',
   },
@@ -19,6 +22,7 @@ export const mockDataBase: Workout[] = [
     id: 3,
     title: 'Szybkie pływanie',
     description: '10 X 50 metrów x 3 serie',
+    type: IWorkoutsType.cardio,
     duration: 45,
     data: '2022-04-11',
   },
@@ -26,6 +30,7 @@ export const mockDataBase: Workout[] = [
     id: 4,
     title: 'Wolne pływanie',
     description: '45 minut zmiennym spokojne tempo',
+    type: IWorkoutsType.cardio,
     duration: 45,
     data: '2022-04-12',
   },
@@ -33,6 +38,7 @@ export const mockDataBase: Workout[] = [
     id: 5,
     title: 'Szybkie bieganie',
     description: '5 serii po 10 sekund sprintu x 3 serie',
+    type: IWorkoutsType.running,
     duration: 15,
     data: '2022-04-11',
   },
