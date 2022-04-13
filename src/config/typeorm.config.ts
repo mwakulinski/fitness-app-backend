07d2457e2b001f02e9acbac83a89c7c68262dfb5
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-13 18:58:03 +0200

    implemention connection to test database

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,14 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
-export const typeOrmConfig: PostgresConnectionOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'postgres',
-  entities: ['dist/**/*.entity.js'],
-  synchronize: true,
+export const typeOrmConfig = (isTest: boolean): PostgresConnectionOptions => {
+  return {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: isTest ? 'fitofit_test' : 'fitofit',
+    entities: [isTest ? 'src/**/*.entity.ts' : 'dist/**/*.entity.js'],
+    synchronize: false,
+  };
 };
