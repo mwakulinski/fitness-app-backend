73d557b4d5059b7ca6f3f4296f5ba337cd578999
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-13 09:38:54 +0200

    add basic validation

--- src/workouts/dto/create-workout.dto.ts
+++ src/workouts/dto/create-workout.dto.ts
@@ -1,9 +1,24 @@
+import { IsString, MinLength } from 'class-validator';
 import { ICreateWorkout, IWorkoutsType } from 'src/interfaces/interfaces';
 
 export class CreateWorkoutDto implements ICreateWorkout {
+  @IsString()
+  @MinLength(5)
   title: string;
+
+  @IsString()
+  @MinLength(5)
   description: string;
+
+  @IsString()
+  @MinLength(5)
   type: IWorkoutsType;
+
+  @IsString()
+  @MinLength(5)
   duration: number;
+
+  @IsString()
+  @MinLength(5)
   data: string;
 }
